8d5c1d057bc275a7deec1b617d2659f3fc6bfdf0
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2025-09-21 02:39:48 +0100

    Switch to Gunicorn in production

--- start-django.sh
+++ start-django.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-set -e
 
 poetry run python manage.py collectstatic --noinput
 poetry run python manage.py migrate
 
 if [[ "$ENV_STATE" == "production" ]]; then
+    echo "Starting with Gunicorn..."
     poetry run gunicorn djangocourse.wsgi --workers 4 --bind 0.0.0.0:8000 --forwarded-allow-ips "*"
 else
+    echo "Starting with Django runserver..."
     poetry run python manage.py runserver 0.0.0.0:8000
 fi
-
-
